1b0bdd0e366d46c4eae500a58fb1f61f41a6d563
avani2011gupta <102972904+avani2011gupta@users.noreply.github.com>
2022-10-07 18:22:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name = "Ginny ";
+console.log(first_name);
+
+var last_name = "Weasley";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 46;
+console.log(a);
+
+var b = 64;
+console.log(b);
+
+var add = b + a ;
+console.log(add);
+
+var subtract = b - a ;
+console.log(subtract);
